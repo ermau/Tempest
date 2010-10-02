5fb7228cb059536a71b58566ad9ae45711a63c9e
ermau <me@ermau.com>
2010-10-01 20:37:16 -0400

    First commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle ("Tempest")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("Eric Maupin")]
+[assembly: AssemblyProduct ("Tempest")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2010")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+[assembly: ComVisible (false)]
+[assembly: Guid ("a4bdac1c-a9d5-4be2-8fb5-14d7210d602f")]
+
+[assembly: AssemblyVersion ("0.0.0.0")]
+[assembly: AssemblyFileVersion ("0.0.0.0")]

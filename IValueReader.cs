8b45745b246307ece3c9af170b766889eaa31ada
ermau <me@ermau.com>
2010-10-01 23:37:51 -0400

    Value reader/writer

--- IValueReader.cs
+++ IValueReader.cs
@@ -20,11 +20,76 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 using System.Linq;
+using System.Text;
 
 namespace Tempest
 {
 	public interface IValueReader
 	{
+		/// <summary>
+		/// Reads a boolean from the transport.
+		/// </summary>
+		bool ReadBool();
+
+		/// <summary>
+		/// Reads an array of unsigned bytes from the transport.
+		/// </summary>
+		byte[] ReadBytes ();
+
+		/// <summary>
+		/// Reads the next <paramref name="count"/> bytes from the transport.
+		/// </summary>
+		/// <param name="count">The number of bytes to read.</param>
+		byte[] ReadBytes (int count);
+
+		/// <summary>
+		/// Reads a signed byte (SByte) from the transport.
+		/// </summary>
+		SByte ReadSByte ();
+
+		/// <summary>
+		/// Reads a signed short (Int16) from the transport.
+		/// </summary>
+		Int16 ReadInt16 ();
+
+		/// <summary>
+		/// Reads a signed integer (Int32) from the transport.
+		/// </summary>
+		Int32 ReadInt32 ();
+
+		/// <summary>
+		/// Reads a signed long (Int64) from the transport.
+		/// </summary>
+		Int64 ReadInt64 ();
+
+		/// <summary>
+		/// Reads an unsigned byte (Byte) from the transport.
+		/// </summary>
+		Byte ReadByte ();
+		
+		/// <summary>
+		/// Reads an unsigned integer (UInt16) from the transport.
+		/// </summary>
+		/// <returns></returns>
+		UInt16 ReadUInt16 ();
+
+		/// <summary>
+		/// Reads an unsigned integer (UInt32) from the transport.
+		/// </summary>
+		/// <returns></returns>
+		UInt32 ReadUInt32 ();
+		
+		/// <summary>
+		/// Reads an unsigned long (UInt64) from the transport.
+		/// </summary>
+		UInt64 ReadUInt64 ();
+		
+		/// <summary>
+		/// Reads a string with <paramref name="encoding"/> from the transport.
+		/// </summary>
+		/// <param name="encoding">The encoding of the string.</param>
+		string ReadString (Encoding encoding);
 	}
 }
--- IValueWriter.cs
+++ IValueWriter.cs
@@ -20,11 +20,97 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
+using System;
 using System.Linq;
+using System.Text;
 
 namespace Tempest
 {
+	/// <summary>
+	/// Serialization contract.
+	/// </summary>
 	public interface IValueWriter
 	{
+		/// <summary>
+		/// Writes an unsigned byte to the transport.
+		/// </summary>
+		/// <param name="value"></param>
+		void WriteByte (byte value);
+		
+		/// <summary>
+		/// Writes a signed byte to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteSByte (sbyte value);
+
+		/// <summary>
+		/// Writes a boolean to the transport.
+		/// </summary>
+		/// <param name="value"></param>
+		void WriteBool (bool value);
+
+		/// <summary>
+		/// Writes an array of unsigned bytes to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		/// <exception cref="ArgumentNullException"><paramref name="value"/> is <c>null</c>.</exception>
+		void WriteBytes (byte[] value);
+
+		/// <summary>
+		/// Writes an array segment to the transport.
+		/// </summary>
+		/// <param name="value">The array to write from.</param>
+		/// <param name="offset">The offset of <paramref name="value"/> to start writing from.</param>
+		/// <param name="length">The length to write from <paramref name="offset"/> in <paramref name="value"/>.</param>
+		/// 
+		void WriteBytes (byte[] value, int offset, int length);
+
+		/// <summary>
+		/// Writes a signed short (Int16) to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteInt16 (Int16 value);
+
+		/// <summary>
+		/// Writes a signed integer (Int32) to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteInt32 (Int32 value);
+		
+		/// <summary>
+		/// Writes a signed long (Int64) to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteInt64 (Int64 value);
+		
+		/// <summary>
+		/// Writes an unsigned short to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteUInt16 (UInt16 value);
+
+		/// <summary>
+		/// Writes an unsigned integer to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteUInt32 (UInt32 value);
+
+		/// <summary>
+		/// Writes an unsigned long to the transport.
+		/// </summary>
+		/// <param name="value">The value to write.</param>
+		void WriteUInt64 (UInt64 value);
+
+		/// <summary>
+		/// Writes a string with <paramref name="encoding"/> to the transport.
+		/// </summary>
+		/// <param name="encoding">The encoding to use.</param>
+		/// <param name="value">The value to write.</param>
+		void WriteString (Encoding encoding, string value);
+
+		/// <summary>
+		/// Flushes any buffered data to the transport.
+		/// </summary>
+		void Flush();
 	}
 }

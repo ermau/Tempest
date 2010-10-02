804439c5e26147228544a457af40169ca6ea3f60
ermau <me@ermau.com>
2010-10-02 01:45:46 -0400

    Changed target framework and StreamValueWriter

---
+++ StreamValueReader.cs
@@ -0,0 +1,92 @@
+﻿// The MIT License
+//
+// Copyright (c) 2010 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Text;
+
+namespace Tempest
+{
+	public class StreamValueReader
+		: IValueReader
+	{
+		public bool ReadBool()
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte[] ReadBytes()
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte[] ReadBytes(int count)
+		{
+			throw new NotImplementedException();
+		}
+
+		public sbyte ReadSByte()
+		{
+			throw new NotImplementedException();
+		}
+
+		public short ReadInt16()
+		{
+			throw new NotImplementedException();
+		}
+
+		public int ReadInt32()
+		{
+			throw new NotImplementedException();
+		}
+
+		public long ReadInt64()
+		{
+			throw new NotImplementedException();
+		}
+
+		public byte ReadByte()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ushort ReadUInt16()
+		{
+			throw new NotImplementedException();
+		}
+
+		public uint ReadUInt32()
+		{
+			throw new NotImplementedException();
+		}
+
+		public ulong ReadUInt64()
+		{
+			throw new NotImplementedException();
+		}
+
+		public string ReadString(Encoding encoding)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}

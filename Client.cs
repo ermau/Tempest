b18cde0787010d0d4388124d45778e7186e61f4f
ermau <me@ermau.com>
2010-10-18 15:32:40 -0400

    First iteration of Client

---
+++ Client.cs
@@ -0,0 +1,302 @@
+﻿//
+// Client.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2010 Eric Maupin
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using Cadenza.Collections;
+
+namespace Tempest
+{
+	public abstract class Client
+		: IContext
+	{
+		protected Client (IClientConnection connection, bool poll)
+		{
+			if (connection == null)
+				throw new ArgumentNullException ("connection");
+
+			this.connection = connection;
+			this.connection.Connected += OnConnectionConnected;
+			this.connection.Disconnected += OnConnectionDisconnected;
+			this.connection.ConnectionFailed += OnConnectionConnectionFailed;
+
+			if (this.connection.Modes.HasFlag (MessagingModes.Inline) && (Environment.ProcessorCount == 1 || !this.connection.Modes.HasFlag (MessagingModes.Async)))
+				this.mode = MessagingModes.Inline;
+			else
+			{
+				this.mqueue = new Queue<MessageReceivedEventArgs>();
+				this.mwait = new AutoResetEvent (false);
+				this.connection.MessageReceived += ConnectionOnMessageReceived;
+				this.mode = MessagingModes.Async;
+			}
+
+			this.polling = poll;
+		}
+
+		public event EventHandler Connected;
+		public event EventHandler ConnectionFailed;
+		public event EventHandler Disconnected;
+
+		/// <summary>
+		/// Gets whether the client is currently connected or not.
+		/// </summary>
+		public virtual bool IsConnected
+		{
+			get { return this.connection.IsConnected; }
+		}
+
+		public abstract void Connect (EndPoint endPoint);
+
+		/// <summary>
+		/// Disconnects from the server.
+		/// </summary>
+		/// <param name="now">Whether or not to disconnect immediately or wait for pending messages.</param>
+		public virtual void Disconnect (bool now)
+		{
+			this.connection.Disconnect (now);
+
+			if (!now)
+			{
+				this.disconnecting = true;
+				return;
+			}
+
+			this.running = false;
+
+			if (this.mode == MessagingModes.Async)
+			{
+				this.mwait.Set();
+					
+				lock (this.mqueue)
+					this.mqueue.Clear();
+			}
+
+			if (this.messageRunner != null)
+			{
+				this.messageRunner.Join();
+				this.messageRunner = null;
+			}
+		}
+
+		public void Poll()
+		{
+			if (!IsConnected)
+				return;
+
+			List<MessageReceivedEventArgs> messages;
+			if (this.mode == MessagingModes.Async)
+			{
+				messages = new List<MessageReceivedEventArgs> (mqueue.Count);
+
+				lock (this.mqueue)
+				{
+					while (this.mqueue.Count > 0)
+						messages.Add (this.mqueue.Dequeue());
+				}
+			}
+			else
+				messages = this.connection.Tick().ToList();
+
+			for (int i = 0; i < messages.Count; ++i)
+			{
+				var m = messages[i];
+				var mhandlers = GetHandlers (m.Message.MessageType);
+				if (mhandlers == null)
+					continue;
+
+				for (int n = 0; n < mhandlers.Count; ++n)
+					mhandlers[n] (m);
+			}
+
+			if (this.disconnecting)
+				Disconnect (true);
+		}
+
+		protected bool disconnecting;
+		protected readonly IClientConnection connection;
+		private readonly MessagingModes mode;
+		private readonly bool polling;
+
+		private readonly MutableLookup<ushort, Action<MessageReceivedEventArgs>> handlers = new MutableLookup<ushort, Action<MessageReceivedEventArgs>>();
+		private readonly Queue<MessageReceivedEventArgs> mqueue;
+		private readonly AutoResetEvent mwait;
+		private Thread messageRunner;
+		protected volatile bool running;	
+
+		protected void Connect (EndPoint endPoint, MessageTypes types)
+		{
+			if (endPoint == null)
+				throw new ArgumentNullException ("endPoint");
+
+			this.connection.Connect (endPoint, types);
+		}
+
+		void IContext.RegisterMessageHandler (ushort messageType, Action<MessageReceivedEventArgs> handler)
+		{
+			lock (this.handlers)
+				this.handlers.Add (messageType, handler);
+		}
+
+		private void ConnectionOnMessageReceived (object sender, MessageReceivedEventArgs e)
+		{
+			lock (this.mqueue)
+				this.mqueue.Enqueue (e);
+
+			this.mwait.Set();
+		}
+
+		private void InlineMessageRunner()
+		{
+			SpinWait wait = new SpinWait();
+
+			while (this.running)
+			{
+				IEnumerable<MessageReceivedEventArgs> messages = this.connection.Tick();
+				while (this.running && messages.Any())
+				{
+					wait.Reset();
+
+					foreach (MessageReceivedEventArgs e in messages)
+					{
+						if (!this.running)
+							break;
+
+						var mhandlers = GetHandlers (e.Message.MessageType);
+						if (mhandlers == null)
+							continue;
+
+						for (int i = 0; i < mhandlers.Count; ++i)
+							mhandlers[i] (e);
+					}
+				}
+
+				if (this.disconnecting)
+				{
+					ThreadPool.QueueUserWorkItem (now => Disconnect ((bool)now), true);
+					return;
+				}
+
+				wait.SpinOnce();
+			}
+		}
+
+		private void AsyncMessageRunner ()
+		{
+			Queue<MessageReceivedEventArgs> q = this.mqueue;
+
+			while (this.running)
+			{
+				while (q.Count > 0)
+				{
+					if (!this.running)
+						break;
+
+					MessageReceivedEventArgs e;
+					lock (q)
+						e = q.Dequeue();
+
+					var mhandlers = GetHandlers (e.Message.MessageType);
+					if (mhandlers == null)
+						continue;
+
+					for (int i = 0; i < mhandlers.Count; ++i)
+						mhandlers[i] (e);
+				}
+
+				if (this.disconnecting)
+				{
+					ThreadPool.QueueUserWorkItem (now => Disconnect ((bool)now), true);
+					return;
+				}
+
+				if (q.Count == 0)
+					this.mwait.WaitOne();
+			}
+		}
+
+		private List<Action<MessageReceivedEventArgs>> GetHandlers (ushort messageType)
+		{
+			List<Action<MessageReceivedEventArgs>> mhandlers = null;
+			lock (this.handlers)
+			{
+				IEnumerable<Action<MessageReceivedEventArgs>> thandlers;
+				if (this.handlers.TryGetValues (messageType, out thandlers))
+					mhandlers = thandlers.ToList();
+			}
+
+			return mhandlers;
+		}
+
+		private void OnConnectionConnectionFailed (object sender, ClientConnectionEventArgs e)
+		{
+			OnConnectionFailed (EventArgs.Empty);
+		}
+
+		private void OnConnectionDisconnected (object sender, ConnectionEventArgs e)
+		{
+			OnDisconnected (EventArgs.Empty);
+		}
+
+		private void OnConnectionConnected (object sender, ClientConnectionEventArgs e)
+		{
+			this.running = true;
+
+			if (!this.polling)
+			{
+				this.messageRunner = (this.mode == MessagingModes.Inline) ? new Thread (InlineMessageRunner) : new Thread (AsyncMessageRunner);
+				this.messageRunner.Name = "Client Message Runner";
+				this.messageRunner.IsBackground = true;
+				this.messageRunner.Start();
+			}
+
+			OnConnected (EventArgs.Empty);
+		}
+
+		protected virtual void OnConnected (EventArgs e)
+		{
+			EventHandler handler = Connected;
+			if (handler != null)
+				handler (this, e);
+		}
+
+		protected virtual void OnConnectionFailed (EventArgs e)
+		{
+			EventHandler handler = ConnectionFailed;
+			if (handler != null)
+				handler (this, e);
+		}
+
+		protected virtual void OnDisconnected (EventArgs e)
+		{
+			EventHandler handler = Disconnected;
+			if (handler != null)
+				handler (this, e);
+		}
+	}
+}
---
+++ Tests/MutableLookupTest.cs
@@ -0,0 +1,437 @@
+//
+// MutableLookupTest.cs
+//
+// Author:
+//   Eric Maupin  <me@ermau.com>
+//
+// Copyright (c) 2010 Eric Maupin (http://www.ermau.com)
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Cadenza.Collections.Tests
+{
+	[TestFixture]
+	public class MutableLookupTest
+	{
+		internal MutableLookup<string, string> GetTestLookup()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, (string)null);
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			return lookup;
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void CtorNull()
+		{
+			new MutableLookup<string, string> ((ILookup<string,string>)null);
+		}
+
+		[Test]
+		[ExpectedException (typeof (ArgumentNullException))]
+		public void CtorEqualityComparerNull()
+		{
+			new MutableLookup<string, string> ((IEqualityComparer<string>)null);
+		}
+
+		[Test]
+		public void CtorILookup()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup <int, int> (ints.ToLookup (i => Int32.Parse (i.ToString()[0].ToString())));
+
+			Assert.AreEqual (3, lookup.Count);
+			Assert.AreEqual (1, lookup[1].Count());
+			Assert.AreEqual (2, lookup[2].Count());
+			Assert.AreEqual (1, lookup[3].Count());
+		}
+
+		[Test]
+		public void CtorILookupWithNulls()
+		{
+			List<string> strs = new List<string> { "Foo", "Foos", "Foobar", "Monkeys", "Bar", "Ban", "Barfoo" };
+
+			var lookup = new MutableLookup<string, string> (strs.ToLookup (s => (s[0] != 'F' && s[0] != 'B') ? null : s[0].ToString()));
+			Assert.AreEqual (3, lookup.Count);
+			Assert.AreEqual (3, lookup["F"].Count());
+			Assert.AreEqual (3, lookup["B"].Count());
+			Assert.AreEqual (1, lookup[null].Count());
+		}
+
+		[Test]
+		public void Add()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add ("F", "Foo");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual ("Foo", lookup["F"].First());
+			lookup.Add ("F", "Foobar");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (2, lookup["F"].Count());
+		}
+		
+		[Test]
+		public void AddNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "Foo");
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (1, lookup[null].Count());
+			lookup.Add (null, (string)null);
+			Assert.AreEqual (1, lookup.Count);
+			Assert.AreEqual (2, lookup[null].Count());
+		}
+
+		[Test]
+		public void AddMultiple()
+		{
+			var values = new [] { "Foo", "Foobar" };
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add ("key", values);
+			Assert.AreEqual (1, lookup.Count);
+			Assert.Contains (values[0], lookup["key"].ToList());
+			Assert.Contains (values[1], lookup["key"].ToList());
+			lookup.Add ("key2", values);
+			Assert.AreEqual (2, lookup.Count);
+			Assert.Contains (values[0], lookup["key2"].ToList());
+			Assert.Contains (values[1], lookup["key2"].ToList());
+		}
+
+		[Test]
+		public void AddMultipleNull()
+		{
+			var values = new [] { "Foo", "Foobar" };
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, values);
+			Assert.AreEqual (1, lookup.Count);
+			Assert.IsTrue (values.SequenceEqual (lookup[null]), "S");
+
+			Assert.Throws<ArgumentNullException> (() => lookup.Add ("foo", (IEnumerable<string>)null));
+		}
+
+		[Test]
+		public void CountRefType()
+		{
+			var lookup = new MutableLookup<string, string>();
+			Assert.AreEqual (0, lookup.Count);
+			lookup.Add (null, "blah");
+			Assert.AreEqual (1, lookup.Count);
+			lookup.Add ("F", "Foo");
+			Assert.AreEqual (2, lookup.Count);
+			lookup.Add ("F", "Foobar");
+			Assert.AreEqual (2, lookup.Count);
+
+			lookup.Remove (null, "blah");
+			Assert.AreEqual (1, lookup.Count);
+		}
+
+		[Test]
+		public void CountValueType()
+		{
+			var lookup = new MutableLookup<int, int>();
+			Assert.AreEqual (0, lookup.Count);
+			lookup.Add (1, 10);
+			Assert.AreEqual (1, lookup.Count);
+			lookup.Add (2, 20);
+			Assert.AreEqual (2, lookup.Count);
+			lookup.Add (2, 21);
+			Assert.AreEqual (2, lookup.Count);
+
+			lookup.Remove (1, 10);
+			Assert.AreEqual(1, lookup.Count);
+		}
+
+		[Test]
+		public void RemoveExisting()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "blah");
+			lookup.Add (null, "monkeys");
+			lookup.Add ("F", "Foo");
+			lookup.Add ("F", "Foobar");
+			lookup.Add ("B", "Bar");
+
+			Assert.AreEqual (3, lookup.Count);
+
+			Assert.IsTrue (lookup.Remove (null, "blah"));
+			Assert.AreEqual (3, lookup.Count);
+			Assert.IsTrue (lookup.Remove (null, "monkeys"));
+			Assert.AreEqual (2, lookup.Count);
+
+			Assert.IsTrue (lookup.Remove ("F"));
+			Assert.AreEqual (1, lookup.Count);
+		}
+
+		[Test]
+		public void RemoveNonExisting()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Remove ("D"));
+			Assert.AreEqual(3, lookup.Count);
+
+			Assert.IsFalse (lookup.Remove ("F", "asdf"));
+			Assert.AreEqual(3, lookup.Count);
+
+			lookup.Remove (null);
+			Assert.IsFalse (lookup.Remove (null));
+			Assert.AreEqual (2, lookup.Count);
+		}
+
+		[Test]
+		public void ClearWithNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add (null, "blah");
+			lookup.Add ("F", "Foo");
+
+			lookup.Clear();
+
+			Assert.AreEqual (0, lookup.Count);
+			Assert.IsFalse (lookup.Contains (null));
+			Assert.IsFalse (lookup.Contains ("F"));
+		}
+
+		[Test]
+		public void ClearWithoutNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			lookup.Clear();
+			Assert.AreEqual (0, lookup.Count);
+			Assert.IsFalse (lookup.Contains ("F"));
+			Assert.IsFalse (lookup.Contains ("B"));
+		}
+
+		[Test]
+		public void ClearValueType()
+		{
+			var lookup = new MutableLookup<int, int>();
+			lookup.Add (1, 10);
+			lookup.Add (1, 12);
+			lookup.Add (1, 13);
+			lookup.Add (2, 21);
+			lookup.Add (2, 22);
+			lookup.Add (2, 23);
+
+			lookup.Clear();
+			Assert.AreEqual (0, lookup.Count);
+			Assert.IsFalse (lookup.Contains (1));
+			Assert.IsFalse (lookup.Contains (2));
+		}
+
+		[Test]
+		public void Contains()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsTrue (lookup.Contains ("B"));
+		}
+
+		[Test]
+		public void DoesNotContain()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add(null, "blah");
+			lookup.Add(null, "monkeys");
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Contains ("D"));
+		}
+
+		[Test]
+		public void ContainsNull()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.IsTrue (lookup.Contains (null));
+		}
+
+		[Test]
+		public void DoesNotContainNull()
+		{
+			var lookup = new MutableLookup<string, string>();
+			lookup.Add("F", "Foo");
+			lookup.Add("F", "Foobar");
+			lookup.Add("B", "Bar");
+
+			Assert.IsFalse (lookup.Contains (null));
+		}
+
+		[Test]
+		public void Indexer()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.AreEqual (2, lookup["F"].Count());
+		}
+
+		[Test]
+		public void IndexerNull()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.AreEqual (3, lookup[null].Count());
+		}
+
+		[Test]
+		public void IndexerNotFound()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.AreEqual (0, lookup["D"].Count());
+		}
+
+		[Test]
+		public void Enumerator()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup<int, int>(ints.ToLookup(i => Int32.Parse(i.ToString()[0].ToString())));
+
+			Assert.AreEqual (3, lookup.Count());
+			Assert.IsTrue (lookup.Any (g => g.Key == 1));
+			Assert.IsTrue (lookup.Any (g => g.Key == 2));
+			Assert.IsTrue (lookup.Any (g => g.Key == 3));
+		}
+
+		[Test]
+		public void EnumeratorNotNull()
+		{
+			List<string> strings = new List<string> { "hi", "hai", "bai", "bye" };
+			var lookup = new MutableLookup<string, string> (strings.ToLookup (s => s[0].ToString()));
+
+			Assert.AreEqual (2, lookup.Count);
+			Assert.IsTrue (lookup.Any (g => g.Key == "h"));
+			Assert.IsTrue (lookup.Any (g => g.Key == "b"));
+			Assert.IsFalse (lookup.Any (g => g.Key == null));
+		}
+
+		[Test]
+		public void EnumeratorNull()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.AreEqual (3, lookup.Count());
+			Assert.IsTrue (lookup.Any (g => g.Key == null));
+			Assert.IsTrue (lookup.Any (g => g.Key == "F"));
+			Assert.IsTrue (lookup.Any (g => g.Key == "B"));
+		}
+
+		[Test]
+		public void NullGroupingEnumerator()
+		{
+			var lookup = GetTestLookup();
+
+			Assert.AreEqual (3, lookup[null].Count());
+			Assert.IsTrue (lookup[null].Any (s => s == "blah"));
+			Assert.IsTrue (lookup[null].Any (s => s == "monkeys"));
+			Assert.IsTrue (lookup[null].Any (s => s == null));
+		}
+
+		[Test]
+		public void GroupingEnumerator()
+		{
+			List<int> ints = new List<int> { 10, 20, 21, 30 };
+			var lookup = new MutableLookup<int, int>(ints.ToLookup(i => Int32.Parse(i.ToString()[0].ToString())));
+
+			Assert.AreEqual (2, lookup[2].Count());
+			Assert.IsTrue (lookup[2].Any (i => i == 20));
+			Assert.IsTrue (lookup[2].Any(i => i == 21));
+		}
+
+		[Test]
+		public void TryGetValuesNull()
+		{
+			var lookup = GetTestLookup();
+
+			IEnumerable<string> values;
+			Assert.IsTrue (lookup.TryGetValues (null, out values));
+			Assert.IsNotNull (values);
+
+			var v = values.ToList();
+			Assert.AreEqual (3, v.Count);
+			Assert.Contains ("blah", v);
+			Assert.Contains ("monkeys", v);
+			Assert.Contains (null, v);
+		}
+
+		[Test]
+		public void TryGetValues()
+		{
+			var lookup = GetTestLookup();
+
+			IEnumerable<string> values;
+			Assert.IsTrue (lookup.TryGetValues ("F", out values));
+			Assert.IsNotNull (values);
+
+			var v = values.ToList();
+			Assert.AreEqual (2, v.Count);
+			Assert.Contains ("Foo", v);
+			Assert.Contains ("Foobar", v);
+		}
+
+		[Test]
+		public void TryGetValuesFail()
+		{
+			var lookup = GetTestLookup();
+
+			IEnumerable<string> values;
+			Assert.IsFalse (lookup.TryGetValues ("notfound", out values));
+			Assert.IsNull (values);
+		}
+
+		[Test]
+		public void TryGetValuesNullFail()
+		{
+			var lookup = new MutableLookup<string,string>();
+
+			IEnumerable<string> values;
+			Assert.IsFalse (lookup.TryGetValues (null, out values));
+			Assert.IsNull (values);
+		}
+	}
+}

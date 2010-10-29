e33c201646ec979ac20fa9b737d95b8d51012589
ermau <me@ermau.com>
2010-10-29 01:06:48 -0400

    Started basic serializer

--- Extensions.cs
+++ Extensions.cs
@@ -26,11 +26,16 @@
 
 using System;
 using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace Tempest
 {
 	public static class Extensions
 	{
+		/// <summary>
+		/// Writes a date value.
+		/// </summary>
 		public static void WriteDate (this IValueWriter writer, DateTime date)
 		{
 			if (writer == null)
@@ -39,12 +44,199 @@ namespace Tempest
 			writer.WriteInt64 (date.ToBinary());
 		}
 
+		/// <summary>
+		/// Reads a date value.
+		/// </summary>
 		public static DateTime ReadDate (this IValueReader reader)
 		{
 			if (reader == null)
 				throw new ArgumentNullException ("reader");
 
 			return DateTime.FromBinary (reader.ReadInt64());
+		}
+
+		public static void Write (this IValueWriter writer, object value)
+		{
+			if (writer == null)
+				throw new ArgumentNullException ("writer");
+
+			writer.WriteBool (value == null);
+			if (value == null)
+				return;
+
+			// TODO: Circular reference handling (hashset passing down the rabit hole?)
+			// TODO: Reflection caching optimization
+			Type t = value.GetType();
+
+			if (t.IsPrimitive)
+			{
+				if (t == typeof (bool))
+					writer.WriteBool ((bool)value);
+				if (t == typeof (byte))
+					writer.WriteByte ((byte)value);
+				else if (t == typeof (sbyte))
+					writer.WriteSByte ((sbyte)value);
+				else if (t == typeof (short))
+					writer.WriteInt16 ((short)value);
+				else if (t == typeof (ushort))
+					writer.WriteUInt16 ((ushort)value);
+				else if (t == typeof (int))
+					writer.WriteInt32 ((int)value);
+				else if (t == typeof (uint))
+					writer.WriteUInt32 ((uint)value);
+				else if (t == typeof (long))
+					writer.WriteInt64 ((long)value);
+				else if (t == typeof (ulong))
+					writer.WriteUInt64 ((ulong)value);
+
+				else if (t == typeof (float))
+					writer.WriteSingle ((float)value);
+				else if (t == typeof (double))
+					writer.WriteDouble ((double)value);
+				else if (t == typeof (decimal))
+					writer.WriteDecimal ((decimal)value);
+
+				return;
+			}
+			else if (t == typeof (DateTime))
+			{
+				writer.WriteDate ((DateTime)(object)value);
+				return;
+			}
+			else if (t == typeof (string))
+			{
+				writer.WriteString (Encoding.UTF8, (string)value);
+				return;
+			}
+			else if (t.IsArray)
+			{
+				Array a = (Array)value;
+				writer.WriteInt32 (a.Length);
+				for (int i = 0; i < a.Length; ++i)
+					writer.Write (a.GetValue (i));
+			}
+			else
+			{
+				MemberInfo[] props =
+					t.GetMembers (BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.GetField)
+						.OrderBy (mi => mi.Name)
+						.ToArray();
+
+				for (int i = 0; i < props.Length; ++i)
+				{
+					if (props[i].MemberType == MemberTypes.Field)
+					{
+						var f = (FieldInfo)props[i];
+						if (f.IsInitOnly)
+							continue;
+
+						Write (writer, f.GetValue (value));
+					}
+					else if (props[i].MemberType == MemberTypes.Property)
+					{
+						var p = (PropertyInfo)props[i];
+						if (!p.CanRead || !p.CanWrite)
+							continue;
+
+						Write (writer, p.GetValue (value, null));
+					}
+				}
+			}
+		}
+
+		public static T Read<T> (this IValueReader reader)
+			where T : new()
+		{
+			return (T)reader.Read (typeof (T));
+		}
+
+		private static object Read (this IValueReader reader, Type type)
+		{
+			if (reader == null)
+				throw new ArgumentNullException ("reader");
+
+			if (reader.ReadBool())
+				return null;
+
+			if (type.IsPrimitive)
+			{
+				if (type == typeof(bool))
+					return reader.ReadBool();
+				if (type == typeof(byte))
+					return reader.ReadByte();
+				if (type == typeof(sbyte))
+					return reader.ReadSByte();
+				if (type == typeof(short))
+					return reader.ReadInt16();
+				if (type == typeof(ushort))
+					return reader.ReadUInt16();
+				if (type == typeof(int))
+					return reader.ReadInt32 ();
+				if (type == typeof(uint))
+					return reader.ReadUInt32();
+				if (type == typeof(long))
+					return reader.ReadInt64();
+				if (type == typeof(ulong))
+					return reader.ReadUInt64();
+				if (type == typeof(short))
+					return reader.ReadString (Encoding.UTF8);
+				if (type == typeof(float))
+					return reader.ReadSingle();
+				if (type == typeof(double))
+					return reader.ReadDouble();
+				if (type == typeof(decimal))
+					return reader.ReadDecimal ();
+
+				throw new ArgumentOutOfRangeException ("type"); // Shouldn't happen.
+			}
+			else if (type == typeof(DateTime))
+				return reader.ReadDate();
+			else if (type == typeof(string))
+				return reader.ReadString (Encoding.UTF8);
+			else if (type.IsArray)
+			{
+				Type etype = type.GetElementType();
+				Array a = Array.CreateInstance (etype, reader.ReadInt32());
+				for (int i = 0; i < a.Length; ++i)
+					a.SetValue (reader.Read (etype), i);
+
+				return a;
+			}
+			else
+			{
+				ConstructorInfo c = type.GetConstructor (Type.EmptyTypes);
+				if (c == null)
+					throw new ArgumentException ("Type must have an empty constructor.", "type");
+
+				object value = c.Invoke (null);
+
+				MemberInfo[] props =
+					type.GetMembers (BindingFlags.Public | BindingFlags.Instance | BindingFlags.GetProperty | BindingFlags.GetField)
+						.OrderBy (mi => mi.Name)
+						.ToArray();
+
+				for (int i = 0; i < props.Length; ++i)
+				{
+					if (props[i].MemberType == MemberTypes.Field)
+					{
+						var f = (FieldInfo)props[i];
+						if (f.IsInitOnly)
+							continue;
+
+						f.SetValue (value, reader.Read (f.FieldType));
+					}
+					else if (props[i].MemberType == MemberTypes.Property)
+					{
+						var p = (PropertyInfo)props[i];
+						if (p.GetSetMethod() == null ||p.GetIndexParameters().Length != 0)
+							continue;
+
+						p.SetValue (value, reader.Read (p.PropertyType), null);
+					}
+				}
+
+				return value;
+			}
 		}
 	}
 }
---
+++ Tests/ExtensionsTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Tempest.Tests
+{
+	[TestFixture]
+	public class ExtensionsTests
+	{
+		[Test]
+		public void ReadWriteDate()
+		{
+			byte[] buffer = new byte[20480];
+			var writer = new BufferValueWriter (buffer);
+
+			DateTime d = DateTime.Now;
+
+			writer.WriteDate (d);
+			writer.Flush();
+
+			var reader = new BufferValueReader (buffer);
+
+			Assert.AreEqual (d, reader.ReadDate());
+		}
+
+		[Test]
+		public void Serializing()
+		{
+			byte[] buffer = new byte[20480];
+			var writer = new BufferValueWriter (buffer);
+
+			var value = new SerializingTester
+			{
+				Child = new SerializingTester
+				{
+					Child = new SerializingTester(true)
+					{
+						Text = "Three",
+						Number = 3,
+					},
+
+					Number = 2,
+					Text = "two",
+					Numbers = new int[] { 3, 2, 1 }
+				},
+
+				Number = 1,
+				Text = "one"
+			};
+
+			writer.Write (value);
+
+			var reader = new BufferValueReader (buffer);
+
+			var readvalue = reader.Read<SerializingTester>();
+			Assert.AreEqual (1, readvalue.Number);
+			Assert.AreEqual ("one", readvalue.Text);
+			Assert.IsFalse (readvalue.IgnoredProperty);
+			Assert.IsNull (readvalue.Numbers);
+			Assert.IsNotNull (readvalue.Child);
+
+			readvalue = readvalue.Child;
+			Assert.AreEqual (2, readvalue.Number);
+			Assert.AreEqual ("two", readvalue.Text);
+			Assert.IsNotNull (readvalue.Numbers);
+			Assert.AreEqual (3, readvalue.Numbers.Length);
+			Assert.AreEqual (3, readvalue.Numbers[0]);
+			Assert.AreEqual (2, readvalue.Numbers[1]);
+			Assert.AreEqual (1, readvalue.Numbers[2]);
+			Assert.IsFalse (readvalue.IgnoredProperty);
+			Assert.IsNotNull (readvalue.Child);
+
+			readvalue = readvalue.Child;
+			Assert.AreEqual (3, readvalue.Number);
+			Assert.AreEqual ("Three", readvalue.Text);
+			Assert.IsNull (readvalue.Numbers);
+			Assert.IsFalse (readvalue.IgnoredProperty);
+			Assert.IsNull (readvalue.Child);
+		}
+
+		public class SerializingTester
+		{
+			public SerializingTester()
+			{
+			}
+
+			public SerializingTester (bool ignored)
+			{
+				IgnoredProperty = ignored;
+			}
+
+			public int Number;
+			public string Text;
+
+			public int[] Numbers
+			{
+				get;
+				set;
+			}
+
+			public SerializingTester Child
+			{
+				get;
+				set;
+			}
+
+			public bool IgnoredProperty
+			{
+				get;
+				private set;
+			}
+		}
+	}
+}

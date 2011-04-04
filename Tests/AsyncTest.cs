95e490cebd73af3a279d1b9f1d5c0856340b6b28
Eric Maupin <me@ermau.com>
2011-04-04 11:25:18 -0400

    Added AsyncTest<T> where T : EventArgs

--- Tests/AsyncTest.cs
+++ Tests/AsyncTest.cs
@@ -34,6 +34,41 @@ using NAssert = NUnit.Framework.Assert;
 
 namespace Tempest.Tests
 {
+	public class AsyncTest<T>
+		: AsyncTest
+		where T : EventArgs
+	{
+		public AsyncTest (Action<T> passTest)
+			: base (e => passTest ((T)e))
+		{
+		}
+
+		public AsyncTest (Action<T> passTest, bool multiple)
+			: base (e => passTest ((T)e), multiple)
+		{
+		}
+
+		public AsyncTest (Action<T> passTest, int times)
+			: base (e => passTest ((T)e), times)
+		{
+		}
+
+		public AsyncTest (Func<T, bool> passPredicate)
+			: base (e => passPredicate ((T)e))
+		{
+		}
+
+		public AsyncTest (Func<T, bool> passPredicate, bool multiple)
+			: base (e => passPredicate ((T)e), multiple)
+		{
+		}
+
+		public AsyncTest (Func<T, bool> passPredicate, int times)
+			: base (e => passPredicate ((T)e), times)
+		{
+		}
+	}
+
 	public class AsyncTest
 	{
 		public AsyncTest()

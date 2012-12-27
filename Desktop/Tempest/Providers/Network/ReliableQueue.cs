ff353933da49348484936f3cb1f3602e0ec7221b
ermau <me@ermau.com>
2012-12-27 12:20:53 -0500

    Add ReliableQueue

---
+++ Desktop/Tempest/Providers/Network/ReliableQueue.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace Tempest.Providers.Network
+{
+	internal class ReliableQueue
+	{
+		public void Clear()
+		{
+			lock (this.queue)
+				this.queue.Clear();
+		}
+
+		public List<MessageEventArgs> Enqueue (MessageEventArgs messageArgs)
+		{
+			var msg = messageArgs.Message;
+			int mid = msg.Header.MessageId;
+
+			if (mid <= this.lastMessageInOrder)
+				return null;
+
+			List<MessageEventArgs> received = null;
+			lock (this.queue)
+			{
+				int d = mid - this.lastMessageInOrder;
+				if (d == 1)
+				{
+					received = new List<MessageEventArgs>();
+					received.Add (messageArgs);
+					
+					int i = 1;
+					for (; i < this.queue.Count; i++)
+					{
+						MessageEventArgs a = this.queue[i];
+						if (a == null)
+							break;
+
+						received.Add (a);
+					}
+
+					if (this.queue.Count > 0)
+						this.queue.RemoveRange (0, i);
+
+					this.lastMessageInOrder = received[received.Count - 1].Message.Header.MessageId;
+				}
+				else
+				{
+					while (d-- > this.queue.Count)
+						this.queue.Add (null);
+
+					var args = this.queue[this.queue.Count - 1];
+					if (args != null)
+					{
+						int largeId = args.Message.Header.MessageId;
+						int pos = (this.queue.Count - 1) - (largeId - mid);
+						this.queue[pos] = messageArgs;
+					}
+					else
+						this.queue.Add (messageArgs);
+				}
+			}
+
+			return received;
+		}
+		
+		// BUG: doesn't handle MID rollover
+		private int lastMessageInOrder;
+		private readonly List<MessageEventArgs> queue = new List<MessageEventArgs>();
+	}
+}
---
+++ Desktop/Tempest/Tests/ReliableQueueTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Tempest.Providers.Network;
+
+namespace Tempest.Tests
+{
+	[TestFixture]
+	public class ReliableQueueTests
+	{
+		private ReliableQueue queue;
+		private IClientConnection connection;
+		
+		[SetUp]
+		public void SetUp()
+		{
+			this.queue = new ReliableQueue();
+
+			var provider = new MockConnectionProvider (MockProtocol.Instance);
+			this.connection = provider.GetClientConnection();
+		}
+
+		[Test]
+		public void EnqueueInOrder()
+		{
+			var first = GetTestMessageArgs (1);
+			var msgs = this.queue.Enqueue (first);
+			Assert.IsNotNull (msgs);
+			CollectionAssert.IsNotEmpty (msgs);
+			CollectionAssert.Contains (msgs, first);
+
+			var second = GetTestMessageArgs (2);
+			msgs = this.queue.Enqueue (second);
+			Assert.IsNotNull (msgs);
+			CollectionAssert.IsNotEmpty (msgs);
+			CollectionAssert.Contains (msgs, second);
+		}
+
+		[TestCase (1, 2)]
+		[TestCase (2, 1)]
+		[TestCase (1, 2, 4, 3)]
+		[TestCase (2, 5, 4, 3, 1)]
+		[TestCase (2, 4, 1, 3)]
+		[TestCase (2, 2, 1)]
+		[TestCase (3, 2, 2, 1)]
+		[TestCase (1, 1, 2)]
+		public void Enqueue (params int[] order)
+		{
+			List<MessageEventArgs> returnedOrder = new List<MessageEventArgs>();
+			for (int i = 0; i < order.Length; i++)
+			{
+				var args = GetTestMessageArgs (order[i]);
+				var results = this.queue.Enqueue (args);
+				if (results != null)
+					returnedOrder.AddRange (results);
+			}
+
+			order = order.OrderBy (i => i).Distinct().ToArray();
+			CollectionAssert.AreEqual (order, returnedOrder.Select (m => m.Message.Header.MessageId).ToArray());
+		}
+
+		private MessageEventArgs GetTestMessageArgs (int mid)
+		{
+			var msg = new BlankMessage();
+			msg.Header = new MessageHeader();
+			msg.Header.MessageId = mid;
+
+			return new MessageEventArgs (this.connection, msg);
+		}
+	}
+}

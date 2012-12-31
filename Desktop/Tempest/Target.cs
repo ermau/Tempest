33590ee41dce7a8ad35cfa254ae0343f9a539223
ermau <me@ermau.com>
2012-12-30 19:31:08 -0500

    Equality and ISerializable for Target

--- Desktop/Tempest/Target.cs
+++ Desktop/Tempest/Target.cs
@@ -29,6 +29,7 @@ using System;
 namespace Tempest
 {
 	public sealed class Target
+		: IEquatable<Target>, ISerializable
 	{
 		public const string AnyIP = "0.0.0.0";
 		public const string AnyIPv6 = "::";
@@ -44,6 +45,14 @@ namespace Tempest
 			Port = port;
 		}
 
+		public Target (ISerializationContext context, IValueReader reader)
+		{
+			if (reader == null)
+				throw new ArgumentNullException ("reader");
+
+			Deserialize (context, reader);
+		}
+
 		public string Hostname
 		{
 			get;
@@ -54,6 +63,56 @@ namespace Tempest
 		{
 			get;
 			private set;
+		}
+
+		public override bool Equals (object obj)
+		{
+			if (ReferenceEquals (null, obj))
+				return false;
+			if (ReferenceEquals (this, obj))
+				return true;
+
+			return obj is Target && Equals ((Target)obj);
+		}
+
+		public bool Equals (Target other)
+		{
+			if (ReferenceEquals (null, other))
+				return false;
+			if (ReferenceEquals (this, other))
+				return true;
+
+			return (String.Equals (Hostname, other.Hostname) && Port == other.Port);
+		}
+
+		public override int GetHashCode()
+		{
+			unchecked
+			{
+				return (Hostname.GetHashCode() * 397) ^ Port;
+			}
+		}
+
+		public static bool operator == (Target left, Target right)
+		{
+			return Equals (left, right);
+		}
+
+		public static bool operator != (Target left, Target right)
+		{
+			return !Equals (left, right);
+		}
+
+		public void Serialize (ISerializationContext context, IValueWriter writer)
+		{
+			writer.WriteString (Hostname);
+			writer.WriteInt32 (Port);
+		}
+
+		public void Deserialize (ISerializationContext context, IValueReader reader)
+		{
+			Hostname = reader.ReadString();
+			Port = reader.ReadInt32();
 		}
 	}
 }

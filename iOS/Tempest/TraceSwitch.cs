bc1836163ebaf741423c9c6a6723c52ed68efd8a
ermau <me@ermau.com>
2013-03-02 03:10:25 -0500

    iOS build

---
+++ iOS/Tempest/TraceSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tempest
+{
+	class TraceSwitch
+	{
+		public TraceSwitch (string foo, string bar)
+		{
+			
+		}
+
+		public bool TraceVerbose
+		{
+			get { return false; }
+		}
+
+		public bool TraceWarning
+		{
+			get { return false; }
+		}
+
+		public bool TraceError
+		{
+			get { return false; }
+		}
+	}
+
+	class Trace
+	{
+		public static void WriteLineIf (bool condition, string message)
+		{
+			
+		}
+
+		public static void WriteLineIf (bool condition, string message, string category)
+		{
+			
+		}
+	}
+}

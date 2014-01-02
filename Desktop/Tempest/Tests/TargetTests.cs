5ea8a9fce9c2b3561be0da042454f3ec6dd0cec2
ermau <me@ermau.com>
2014-01-02 15:30:46 -0500

    [Tests] Add more Target tests

--- Desktop/Tempest/Tests/TargetTests.cs
+++ Desktop/Tempest/Tests/TargetTests.cs
@@ -4,7 +4,7 @@
 // Author:
 //   Eric Maupin <me@ermau.com>
 //
-// Copyright (c) 2012 Eric Maupin
+// Copyright (c) 2012-2014 Eric Maupin
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -25,6 +25,8 @@
 // THE SOFTWARE.
 
 using System;
+using System.Net;
+using System.Net.Sockets;
 using NUnit.Framework;
 
 namespace Tempest.Tests
@@ -36,6 +38,62 @@ namespace Tempest.Tests
 		public void CtorNull()
 		{
 			Assert.Throws<ArgumentNullException> (() => new Target (null, 0));
+		}
+
+		[Test]
+		public void Ctor()
+		{
+			var target = new Target ("host", 1234);
+			Assert.That (target.Hostname, Is.EqualTo ("host"));
+			Assert.That (target.Port, Is.EqualTo (1234));
+		}
+
+		// Extensions
+
+		[Test]
+		public void ToEndPointNull()
+		{
+			Assert.That (() => TargetExtensions.ToEndPoint (null), Throws.TypeOf<ArgumentNullException>());
+		}
+
+		[Test]
+		public void ToEndPointIPv4()
+		{
+			var target = new Target ("127.0.0.1", 1234);
+			EndPoint endPoint = target.ToEndPoint();
+
+			Assert.That (endPoint, Is.InstanceOf<IPEndPoint>());
+
+			IPEndPoint ip = (IPEndPoint) endPoint;
+			Assert.That (ip.Address, Is.EqualTo (IPAddress.Parse ("127.0.0.1")));
+			Assert.That (ip.AddressFamily, Is.EqualTo (AddressFamily.InterNetwork));
+		}
+
+		[Test]
+		public void ToEndPointIPv6()
+		{
+			var target = new Target ("::1", 1234);
+			EndPoint endPoint = target.ToEndPoint();
+
+			Assert.That (endPoint, Is.InstanceOf<IPEndPoint>());
+
+			IPEndPoint ip = (IPEndPoint) endPoint;
+			Assert.That (ip.Address, Is.EqualTo (IPAddress.Parse ("::1")));
+			Assert.That (ip.AddressFamily, Is.EqualTo (AddressFamily.InterNetworkV6));
+			Assert.That (ip.Port, Is.EqualTo (target.Port));
+		}
+
+		[Test]
+		public void ToEndPointHostname()
+		{
+			var target = new Target ("localhost", 1234);
+			EndPoint endPoint = target.ToEndPoint();
+
+			Assert.That (endPoint, Is.InstanceOf<DnsEndPoint>());
+
+			DnsEndPoint dns = (DnsEndPoint) endPoint;
+			Assert.That (dns.Host, Is.EqualTo (target.Hostname));
+			Assert.That (dns.Port, Is.EqualTo (target.Port));
 		}
 	}
 }

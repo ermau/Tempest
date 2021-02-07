ef6283d1eeb6530ba5bfc7d2e7c98a59096b2317
Eric Maupin <me@ermau.com>
2021-02-06 21:36:57 -0500

    Fix tracing in tests

---
+++ Tempest.Tests/SetupFixture.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+using Tempest.Providers.Network;
+
+namespace Tempest.Tests
+{
+	[SetUpFixture]
+	public class SetupFixture
+	{
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			Trace.Listeners.Add (new ImmediateOutput ());
+
+			// .NET Core doesn't support .config
+			NetworkConnection.NTrace.Level = TraceLevel.Verbose;
+		}
+
+		private class ImmediateOutput
+			: TraceListener
+		{
+			public override void Write (string message)
+			{
+				TestContext.Write (message);
+			}
+
+			public override void WriteLine (string message)
+			{
+				TestContext.WriteLine (message);
+			}
+		}
+	}
+}
